1c3cb5effbbcafe659805c8d95dda0f00053a97f
Akmulla <d4n73@yandex.ru>
2016-07-07 15:08:35 -0500

    commit

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMove : MonoBehaviour 
+{
+	public float speed;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		rb.velocity = new Vector3 (1, 0, 0)*speed;
+	}
+}
---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour 
+{
+	public GameObject ball;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = ball.transform.position+new Vector3(-3,3,0);
+	}
+}
